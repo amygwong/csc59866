3c846ac3a888f56e868980f5a84753bfcb7144cd
Shawn <shawn@CEN-0031736.local>
2017-02-07 22:43:47 -0500

    Create experiments folder just to try out new smaller functions

---
+++ experiments/dist_exp.py
@@ -0,0 +1,9 @@
+phrase1 = "Open the folder"
+phrase2 = "Can you please open folder"
+phrase3 = "Create a new folder"
+phrase4 = "Create a new text file"
+
+#Common word check
+for i in phrase1.split():
+    print i
+
