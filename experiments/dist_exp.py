89623742244ed88b560f4aef0fac8cb472b61b74
Shawn <shawn@CEN-0031736.local>
2017-02-20 22:21:57 -0500

    Experimenting with cosine similarity

--- experiments/dist_exp.py
+++ experiments/dist_exp.py
@@ -1,10 +1,31 @@
-phrase1 = "Open the folder"
-phrase2 = "Can you please open folder"
-phrase3 = "Create a new folder"
-phrase4 = "Create a new text file"
+import re, math
+from collections import Counter
 
-#Common word check
-for i in phrase1.split():
-    for word in phrase2.split():
-        if i.lower() == word.lower():
-              
+WORD = re.compile(r'\w+')
+
+def get_cosine(vec1, vec2):
+     intersection = set(vec1.keys()) & set(vec2.keys())
+     numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+     sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+     sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+     if not denominator:
+        return 0.0
+     else:
+        return float(numerator) / denominator
+
+def text_to_vector(text):
+     words = WORD.findall(text)
+     return Counter(words)
+
+text1 = 'Open the folder .'
+text2 = 'Create a new text file.'
+
+vector1 = text_to_vector(text1)
+vector2 = text_to_vector(text2)
+
+cosine = get_cosine(vector1, vector2)
+
+print 'Cosine:', cosine 
