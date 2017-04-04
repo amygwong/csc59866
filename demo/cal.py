922febefdc21eb685974b4b497f340cdbae17d4a
Shawn <shawn@CEN-0031736.local>
2017-04-04 16:35:31 -0400

    Forgot to add files

---
+++ demo/cal.py
@@ -0,0 +1,84 @@
+from os import system
+
+
+
+
+def makeEvent():
+    system('say What description would you like for your event?')
+    desc = input("What description would you like for your event?")
+
+    system('say What summary would you like for your event?')
+    summ = input("What description would you like for your event?")
+
+    system('say Where will this event take place?')
+    loc = input("Where will this event take place? ")
+
+    cmd = """osascript -e'Tell application "Calendar"
+         activate
+         tell calendar "Home"
+              set theCurrentDate to current date
+              make new event at end with properties {description:"%s", summary:"%s", location:"%s", start date:theCurrentDate, end date:theCurrentDate +120 * Minutes}
+         end tell
+         reload calendars
+    end Tell'
+
+    """ % (desc, summ, loc)
+
+    system(cmd)
+
+
+
+#Function for making events
+def makeEvents(desc, summ, loc):
+
+    cmd = """osascript -e'Tell application "Calendar"
+         activate
+         tell calendar "Home"
+              set theCurrentDate to current date
+              make new event at end with properties {description:"%s", summary:"%s", location:"%s", start date:theCurrentDate, end date:theCurrentDate +120 * Minutes}
+         end tell
+         reload calendars
+    end Tell'
+
+    """ % (desc, summ, loc)
+
+    system(cmd)
+
+
+#Function for listing the start times of each event for the day
+def listEvents():
+    cmd = """osascript -e'property my_calendar : "Home"
+
+    set my_date to (current date) -- or any other date you want
+
+    copy my_date to Start_Date
+    set time of Start_Date to 0
+    copy my_date to End_Date
+    set time of End_Date to 86399 -- 23:59:59 in seconds
+
+    tell application "Calendar"
+    tell calendar my_calendar
+        -- read all events betwen start and end dates
+        set my_List to (every event whose (start date is greater than or equal to Start_Date) and (start date is less than or equal to End_Date))
+
+        repeat with myEvent in my_List -- loop through each event
+            --do something
+            log (start date of myEvent) as string
+        end repeat
+    end tell --Calendar
+    end tell -- application'
+
+    """
+
+    system(cmd)
+
+
+
+
+system('say Hi, Did you say you wanted to make an event? ')
+reply = input("Hi, Did you say you wanted to make an event?")
+
+if reply == "yes":
+    makeEvent()
+else:
+    system('say OK, maybe some other time then.')
