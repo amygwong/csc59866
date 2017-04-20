c0d28f57ca76411df42fc7cc5b658a5b22744074
Shawn <shawn@CEN-0031736.local>
2017-04-20 11:18:30 -0400

    Set up the passive checking

--- demo/demo.py
+++ demo/demo.py
@@ -5,24 +5,40 @@ from os import system
 from commands import commands
 from actions import action
 from speech_to_text import getUserInput
+import time
 
+#setting everything up for the loop
 a = commands()
 inp = ""
 val = -1
+startTime = time.time()
+
+
 
 while True:
     sys_says = "Command me"
         #while inp == "" :
-    inp = getUserInput(sys_says)
+    inp = input()
+    """inp = getUserInput(sys_says)
     while inp == "" or inp == -1:
         inp = getUserInput("Try Again")
-    if inp == "quit":
+    #handle quiting case    
+    if inp == "quit" or inp == "quick":
         break
+        """
     #handle the command classification    
     val = a.getCommand(inp)
     print(val)
+    #do the command
     action(val)
-    #create function to handle execution
+    print(time.time()-startTime)
+    if time.time()-startTime > 120:
+        #do passive checking here
+        print("Passive check now")
+        
+        #reset the clock
+        startTime = time.time()
+    
 
 
 
