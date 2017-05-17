ac70e743557eff00decb9a5d9508030144ba17a1
Shawn <shawn@CEN-0031736.local>
2017-05-16 23:38:37 -0400

    some voice recognition stuff

--- demo/wavToFreq.py
+++ demo/wavToFreq.py
@@ -1,20 +1,28 @@
 from scipy.io import wavfile
 import numpy as np
 from scipy import stats
+from scipy.fftpack import fft
+import wave
 
 filename = "microphone-results.wav"
+
 sample_rate, data = wavfile.read(filename)
 
-w = np.fft.fft(data)
-freqs = np.fft.fftfreq(len(data))
-for f in freqs:
-    f = f / 1000
-    print(f)
+
+fs, data = wavfile.read(filename) # load the data
+a = data # this is a two channel soundtrack, I get the first track
+b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
+c = fft(b) # calculate fourier transform (complex numbers list)
+d = int(len(c)/2)
+
+freqs = abs(c[:(d-1)])
+freqs = [i/1000 for i in freqs if i < 280]
 
 meanfreq = np.mean(freqs)
 sd = np.std(freqs)
 median = np.median(freqs)
 q75, q25 = np.percentile(freqs, [75 ,25])
-iqr = q75 - q25
-skew = stats.skew(freqs)
-kurt = stats.kurtosis(freqs)
+
+print(meanfreq,sd,median,q25,q75)
+
+
---
+++ experiments/applicationOpening/keyPressing.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.externals import joblib
+
+
+#Read the voice dataset
+mydata = pd.read_csv("voice/voice.csv")
+
+#Preview voice dataset
+mydata.head()
+print(mydata.shape)
+
+
+
+#Prepare data for modeling
+mydata.loc[:,'label'][mydata['label']=="male"] = 0
+mydata.loc[:,'label'][mydata['label']=="female"] = 1
+mydata_train, mydata_test = train_test_split(mydata, random_state=0, test_size=.2)
+scaler = StandardScaler()
+scaler.fit(mydata_train.ix[:,0:5])
+X_train = scaler.transform(mydata_train.ix[:,0:5])
+X_test = scaler.transform(mydata_test.ix[:,0:5])
+y_train = list(mydata_train['label'].values)
+y_test = list(mydata_test['label'].values)
+
+print(mydata_train.ix[:,0:5])
+
+#train the data
+svm = SVC().fit(X_train, y_train)
+print("Support Vector Machine")
+print("Accuracy on training set: {:.3f}".format(svm.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(svm.score(X_test, y_test)))
+
+#save the data into a file
+joblib.dump(svm, 'svm.pkl') 
+joblib.dump(scaler, 'scaler.pkl')
+
+print("We done")
